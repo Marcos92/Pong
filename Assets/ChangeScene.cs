a47ad88cebf4f331d2f8d3143da7edd89edf2f44
liptonshell <carlitos_260793@hotmail.com>
2016-04-23 23:25:28 +0100

    UI Simplificada
    UI adicionada
    scene01 o suposto menu inicial
    scene02 contador tempo e um espaço para score

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+
+	// Update is called once per frame
+	public void ChangeToScene (string MudarCena) {
+		Application.LoadLevel (MudarCena);
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Assets/CountDown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour {
+
+	public Text CounterText;
+
+	public float segundos,minutos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		minutos = (int)(Time.time / 60f);
+		segundos = (int)(Time.time % 60f);
+		CounterText.text = minutos.ToString("00") + ":" + segundos.ToString("00");
+
+	}
+}
