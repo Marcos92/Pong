7f0542e278d5c53c90373cb68e08632d747e8d47
MarcosIPCA <marcos.mgbr@gmail.com>
2016-05-04 15:58:26 +0000

    Strike e Dash

--- Assets/Scripts/Pong.cs
+++ Assets/Scripts/Pong.cs
@@ -14,6 +14,23 @@ public class Pong : MonoBehaviour
 
     public int points;
 
+    //Bots
+    public bool bot = false;
+    Transform nearestBall;
+    public float delay;
+    float nextSearchTime;
+
+    //Strike
+    [HideInInspector]
+    public bool striking = false;
+    public float strikeDuration = 1f;
+    public float strikePower;
+
+    //Dash
+    bool dashing = false;
+    public float dashDuration = 0.5f;
+    public float dashSpeedMultiplier = 1.2f;
+
 	void Start ()
     {
         //rigidBody = GetComponent<Rigidbody>();
@@ -36,7 +53,9 @@ public class Pong : MonoBehaviour
             zMin = zMax;
             zMax = aux;
         }
-        print(bounds);
+        //print(bounds);
+
+        nextSearchTime = Time.time;
     }
     float xMin, xMax, zMin, zMax;
     public static float myBounds;
@@ -45,12 +64,57 @@ public class Pong : MonoBehaviour
         if(controlable) direction = (int)Input.GetAxisRaw("Horizontal");
 
         float velocity = direction * Time.deltaTime * speed;
-        transform.Translate(transform.right * velocity, transform.parent);
+
+        if (bot) 
+        {
+            if (Time.time > nextSearchTime)
+            {
+                nextSearchTime = Time.time + delay;
+
+                //Guardar posição da bola mais próxima
+            }
+            
+            //Movimento para a direcção guardada
+        }
+        else transform.Translate(transform.right * velocity, transform.parent);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Input.GetAxisRaw("Horizontal") == 0)
+            {
+                //Strike
+                StartCoroutine("Strike");
+            }
+            else
+            {
+                //Dash
+                StartCoroutine("Dash", Input.GetAxisRaw("Horizontal"));
+            }
+        }
 
         Vector3 pos = transform.localPosition;
         pos.x = Mathf.Clamp(transform.localPosition.x, xMin, xMax);
         pos.z = Mathf.Clamp(transform.localPosition.z, zMin, zMax);
         transform.localPosition = pos;
+    }
+
+    IEnumerator Strike()
+    {
+        striking = true;
+        yield return new WaitForSeconds(1f);
+        striking = false;
+    }
+
+    IEnumerator Dash(float d)
+    {
+        float endDashTime = Time.time + dashDuration;
+        float v = d * Time.deltaTime * speed * dashSpeedMultiplier;
+
+        while(Time.time < endDashTime)
+        {
+            transform.Translate(transform.right * v, transform.parent);
+            yield return null;
+        }
     }
 
     //void OnTriggerEnter(Collider collider)
