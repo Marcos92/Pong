3fbfce49c84a2d3c171cf97c061864198ed7c685
Marcos92 <marcos.mgbr@gmail.com>
2016-06-10 22:03:34 +0100

    Core shader & ball trail

---
+++ Assets/Shaders/CoreShader/ParticleGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleGenerator : MonoBehaviour
+{
+    public int boundsX, boundsZ;
+    public SphericalFog particle;
+    public float timeBetweenParticles;
+    float nextParticleSpawn;
+
+	void Start ()
+    {
+        nextParticleSpawn = Time.time + timeBetweenParticles;
+	}
+	
+	void Update ()
+    {
+	    if(Time.time >= nextParticleSpawn)
+        {
+            nextParticleSpawn = Time.time + timeBetweenParticles;
+
+            SphericalFog p = Instantiate(particle, new Vector3(Random.Range(-boundsX, boundsX), 0, Random.Range(-boundsZ, boundsZ)), transform.rotation) as SphericalFog;
+        }
+	}
+}
